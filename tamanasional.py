8ea0a2643afdd6effec8524719491ced74dc671c
arifams <arif.amsterdam@gmail.com>
2023-07-28 13:55:05 +0200

    Create tamanasional.py

---
+++ tamanasional.py
@@ -0,0 +1,14 @@
+import random
+
+# list to just popping up national park and endemic animal names
+
+endemik_list = ["Anoa dataran rendah", "Anoa pegunungan", "Arwana emas", "Babirusa", "Badak jawa", "Badak sumatra", "Bajing palawan", "Bajing tanah", "Bajing telinga botol", "Banteng jawa", "Bekantan", "Beruk mentawai", "Burung cendrawasih", "Burung maleo", "Harimau sumatra", "Hiu karpet berbintik", "Jalak Bali", "Kadal coklat kalimantan", "Kambing hutan sumatra", "Kancil jawa", "Kanguru pohon mantel emas", "Kasuari gelambir tunggal", "Kasuari kerdil", "Katak tanpa paru-paru", "Kelelawar berjenggot coklat", "Kelinci belang sumatra", "Kera hitam sulawesi", "Kodok darah", "Kodok pohon ungaran", "Komodo", "Kucing merah", "Kukang jawa", "Kura-kura leher ular", "Kuskus beruang", "Kuskus gebe", "Kuskus kerdil", "Kuskus mata biru", "Kuskus obi", "Landak Borneo", "Landak jawa", "Landak sumatra", "Lutung", "Lutung dahi putih", "Lutung merah", "Macan dahan", "Macan tutul jawa", "Meong congkok", "Monyet ekor babi", "Musang sulawesi", "Nuri Sayap Hitam", "Orang utan kalimantan", "Orang utan sumatra", "Owa jawa", "Owa-owa", "Penyu belimbing", "Penyu hijau", "Penyu lekang", "Penyu sisik", "Pesut", "Rangkong papan", "Rusa bawean", "Rusa Timor", "Siamang", "Surili", "Tarsius bangka", "Tarsius peleng", "Tarsius pygmy", "Tarsius sangir", "Tarsius sulawesi", "Tupai Mentawai"]
+
+endemik = random.choice(endemik_list)
+print(endemik)
+
+
+tamanasional_list = ["Bali Barat", "Gunung Rinjani", "Kelimutu", "Komodo", "Laiwangi-Wanggameti", "Manupeu-Tanah Daru", "Tambora", "Alas Purwo", "Baluran", "Bromo Tengger Semeru", "Gunung Ciremai", "Gunung Gede Pangrango", "Gunung Halimun-Salak", "Gunung Merapi", "Gunung Merbabu", "Gunung Sanggabuana", "Karimunjawa", "Kepulauan Seribu", "Meru Betiri", "Ujung Kulon", "Betung Kerihun", "Bukit Baka Bukit Raya", "Danau Sentarum", "Gunung Palung", "Kayan Mentarang", "Kutai", "Sebangau", "Tanjung Puting", "Aketajawe-Lolobata", "Lorentz", "Manusela", "Teluk Cenderawasih", "Wasur", "Bantimurung-Bulusaraung", "Bogani Nani Wartabone", "Bunaken", "Kepulauan Togean", "Lore Lindu", "Rawa Aopa Watumohai", "Taka Bonerate", "Wakatobi", "Gunung Gandangdewata", "Batang Gadis", "Berbak", "Bukit Barisan Selatan", "Bukit Duabelas", "Bukit Tiga Puluh", "Gunung Leuser", "Kerinci Seblat", "Sembilang", "Siberut", "Tesso Nilo", "Way Kambas", "Zamrud", "Gunung Maras"]
+
+tamanasional = random.choice(tamanasional_list)
+print(tamanasional)
